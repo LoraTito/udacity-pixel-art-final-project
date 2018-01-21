7ecf342c4a18cc068d93e039095b7679ccffb2d2
Lora <lora@seemelissa.com>
2018-01-21 13:08:18 +0200

    Work on create table function

--- designs.js
+++ designs.js
@@ -6,5 +6,22 @@
 function makeGrid() {
 
 // Your code goes here!
+const height = $('#input_height').val();
+const width = $('#input_width').val();
+const table = $('#pixel_canvas');
 
+for(var row = 0; row < height; row++){
+    var tr = document.createElement('tr');
+    table.append(tr);
 }
+
+    for(var col = 0; col < width; col++){
+        var td = document.createElement('td');
+        table.append(td);
+    }
+}
+
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    makeGrid();
+});
