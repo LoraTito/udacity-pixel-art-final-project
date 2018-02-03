4d86ae10a0b61d8f5414f00420ab49ab9c3d9d87
Lora <lora@seemelissa.com>
2018-02-03 15:52:22 +0200

    Fix grid bug

--- designs.js
+++ designs.js
@@ -6,18 +6,18 @@
 function makeGrid() {
 
 // Your code goes here!
-const height = $('#input_height').val();
-const width = $('#input_width').val();
-const table = $('#pixel_canvas');
+    const height = $('#input_height').val();
+    const width = $('#input_width').val();
+    const table = $('#pixel_canvas');
 
-for(var row = 0; row < height; row++){
-    var tr = document.createElement('tr');
-    table.append(tr);
-}
+    for (var row = 0; row < height; row++) {
+        var tr = document.createElement('tr');
+        table.append(tr);
 
-    for(var col = 0; col < width; col++){
-        var td = document.createElement('td');
-        table.append(td);
+        for (var col = 0; col < width; col++) {
+            var td = document.createElement('td');
+            tr.append(td);
+        }
     }
 }
 
